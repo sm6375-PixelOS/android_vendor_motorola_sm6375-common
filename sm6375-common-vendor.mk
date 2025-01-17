c7a64827fc72472fdebbc30dc146a7a0214a83d8
Michael Bestas <mikeioannina@gmail.com>
2025-01-18 00:21:04 +0530

    sm6375-common: fix vendor.qti.hardware.fm@1.0 ELF checks
    Change-Id: I43f0824f88c24dd9d039f9075090c17f4266ab2d

--- sm6375-common-vendor.mk
+++ sm6375-common-vendor.mk
@@ -652,7 +652,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.data.lce@1.0 \
     vendor.qti.hardware.data.qmi@1.0 \
     vendor.qti.hardware.eid@1.0 \
-    vendor.qti.hardware.fm@1.0 \
+    vendor.qti.hardware.fm@1.0-vendor \
     vendor.qti.hardware.mwqemadapter@1.0 \
     vendor.qti.hardware.qteeconnector@1.0 \
     vendor.qti.hardware.radio.am@1.0 \
