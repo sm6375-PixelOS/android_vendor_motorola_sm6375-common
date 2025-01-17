57362fb88288b55d15c2064b5211a782f516dd97
Demon000 <demonsingur@gmail.com>
2025-01-18 00:23:45 +0530

    sm6375-common: fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09

--- sm6375-common-vendor.mk
+++ sm6375-common-vendor.mk
@@ -714,7 +714,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsuce@1.1 \
     vendor.qti.ims.rcsuce@1.2 \
     vendor.qti.imsrtpservice@3.0-service-Impl \
-    vendor.qti.imsrtpservice@3.0 \
+    vendor.qti.imsrtpservice@3.0-vendor \
     vendor.qti.latency@2.0 \
     vendor.qti.latency@2.1 \
     bm2n06 \
